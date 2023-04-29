e7ad52c6b523549c5be1e55799cdafd4e3da5c03
Michael Nadeau <waaverecords@gmail.com>
2023-04-29 19:39:37 -0400

    Under construction landing page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+        lineHeight: {
+            '11': '2.75rem',
+            '12': '3rem',
+            '13': '3.25rem',
+            '14': '3.5rem',
+            '15': '3.75rem'
+        }
     },
   },
   plugins: [],
